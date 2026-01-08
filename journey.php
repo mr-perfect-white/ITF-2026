70c64507cc23dc69095de2daf0428651d27e5151
Sathvika S Reddy <sathvika@example.com>
2026-01-08 19:00:04 +0530

    journey page and images upadted

---
+++ journey.php
@@ -0,0 +1,187 @@
+<?php require_once 'inc/header.php'; ?>
+
+<!-- About Section Start -->
+
+
+  <!-- Service Section Start -->
+        <section class="service-section section-padding section-bg">
+            <div class="top-shape">
+                <img src="../ITF/frontend/img/home-1/service/top-shape.png" alt="img">
+            </div>
+            <div class="service-left-shape d-none d-xxl-block float-bob-y">
+                <img src="../ITF/frontend/img/home-1/service/left-shape.png" alt="img">
+            </div>
+            <div class="service-right-shape d-none d-xxl-block float-bob-x">
+                <img src="../ITF/frontend/img/home-1/service/right-shape.png" alt="img">
+            </div>
+            <div class="container">
+                <div class="section-title text-center">
+                    <span class="wow fadeInUp"><img src="../ITF/frontend/img/sub-title.svg" alt="img"> Our journey</span>
+                    <h2 class="text-anim">ITF Journey</h2>
+                    <p class="wow fadeInUp" data-wow-delay=".2s">
+                      The Sixth edition of Agriculture beyond cultivation and empowering farmers , 2025 – International Trade Fair is organized by
+                            the Department of Agriculture, Government Of Karnataka. It will be a platform for farmers,
+                            farmer groups, domestic and international companies, central and state institutions in the
+                            organic and millet sector to connect and explore opportunities ain Agri-horticulture,
+                            processing, machinery and Agri-technology.
+
+                            The Government of Karnataka has been a leader in the promotion of organics and millets with
+                            the First Organics and Millets trade fair being held in 2017, and subsequently the second,
+                            third and fourth editions in 2018, 2019 ,2023 and 2024 respectively in Bengaluru.
+                    </p>
+                </div>
+                <div class="service-wrapper">
+                     <ul class="nav">
+                        <li class="nav-item wow fadeInUp" data-wow-delay=".2s">
+                            <a href="#thumb1" data-bs-toggle="tab" class="nav-link active fw-bold">
+                           2017
+                            </a>
+                        </li>
+                        <li class="nav-item wow fadeInUp" data-wow-delay=".4s">
+                            <a href="#thumb2" data-bs-toggle="tab" class="nav-link fw-bold">
+                               2018
+                            </a>
+                        </li>
+                        <li class="nav-item wow fadeInUp" data-wow-delay=".6s">
+                            <a href="#thumb3" data-bs-toggle="tab" class="nav-link fw-bold">
+                          2019
+                            </a>
+                        </li>
+                        <li class="nav-item wow fadeInUp" data-wow-delay=".8s">
+                            <a href="#thumb4" data-bs-toggle="tab" class="nav-link fw-bold">
+                           2023
+                            </a>
+                        </li>
+                        <li class="nav-item wow fadeInUp" data-wow-delay=".8s">
+                            <a href="#thumb5" data-bs-toggle="tab" class="nav-link fw-bold">
+                           2024
+                            </a>
+                        </li>
+                        <!-- <li class="nav-item wow fadeInUp" data-wow-delay=".8s">
+                            <a href="#thumb4" data-bs-toggle="tab" class="nav-link">
+                           2025
+                            </a>
+                        </li> -->
+                    </ul>
+                     <div class="tab-content">
+                        <div id="thumb1" class="tab-pane fade show active">
+                            <div class="row align-items-center g-4">
+                                <div class="col-lg-6 wow fadeInUp" data-wow-delay=".3s">
+                                    <div class="service-content-area">
+                                        <div class="icon">
+                                            <i class="flaticon-agriculture"></i>
+                                        </div>
+                                        <div class="content">
+                                            <h3><a href="service-details.html">Agriculture beyond cultivation and empowering farmers</a></h3>
+                                            <p>2017-National Trade Fair during April 28-30, 2017 at Tripuravasini Palace Grounds, Bengaluru.</p>
+                                            <!-- <a href="service-details.html" class="theme-btn">
+                                                View Details <i class="far fa-arrow-right"></i>
+                                            </a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 wow fadeInUp" data-wow-delay=".5s">
+                                    <div class="service-image">
+                                        <img src="../ITF/frontend/img/ITF-2017.jpg" alt="img">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="thumb2" class="tab-pane fade">
+                            <div class="row align-items-center g-4">
+                                <div class="col-lg-6">
+                                    <div class="service-content-area">
+                                        <div class="icon">
+                                            <i class="flaticon-agriculture"></i>
+                                        </div>
+                                        <div class="content">
+                                            <h3><a href="service-details.html">Agriculture beyond cultivation and empowering farmers</a></h3>
+                                            <p>2018 – International Trade Fair during Jan 19-21, 2018 at Tripuravasini Palace Grounds, Bengaluru</p>
+                                            <!-- <a href="service-details.html" class="theme-btn">
+                                                View Details <i class="far fa-arrow-right"></i>
+                                            </a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="service-image">
+                                        <img src="../ITF/frontend/img/ITF-2108.jpg" alt="img">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="thumb3" class="tab-pane fade">
+                             <div class="row align-items-center g-4">
+                                <div class="col-lg-6">
+                                    <div class="service-content-area">
+                                        <div class="icon">
+                                            <i class="flaticon-agriculture"></i>
+                                        </div>
+                                        <div class="content">
+                                            <h3><a href="service-details.html">Agriculture beyond cultivation and empowering farmers</a></h3>
+                                            <p>Wendell Berry said it best when he said that “eating is an agricultural act.” Our mission is to change the food system by providing a healthy, fresh alternative to industrial food.</p>
+                                            <!-- <a href="service-details.html" class="theme-btn">
+                                                View Details <i class="far fa-arrow-right"></i>
+                                            </a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="service-image">
+                                        <img src="../ITF/frontend/img/history19.jpg" alt="img">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="thumb4" class="tab-pane fade">
+                            <div class="row align-items-center g-4">
+                                <div class="col-lg-6">
+                                    <div class="service-content-area">
+                                        <div class="icon">
+                                            <i class="flaticon-agriculture"></i>
+                                        </div>
+                                        <div class="content">
+                                            <h3><a href="service-details.html">Agriculture beyond cultivation and empowering farmers</a></h3>
+                                           <p>2023- International trade Fair, Jan 18-20, 2023 at Tripuravasini Palace Grounds, Bengaluru.</p>
+                                            <!-- <a href="service-details.html" class="theme-btn">
+                                                View Details <i class="far fa-arrow-right"></i>
+                                            </a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="service-image">
+                                        <img src="../ITF/frontend/img/ITF-2023.jpg" alt="img">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="thumb5" class="tab-pane fade">
+                            <div class="row align-items-center g-4">
+                                <div class="col-lg-6">
+                                    <div class="service-content-area">
+                                        <div class="icon">
+                                            <i class="flaticon-agriculture"></i>
+                                        </div>
+                                        <div class="content">
+                                            <h3><a href="service-details.html">Agriculture beyond cultivation and empowering farmers</a></h3>
+                                            <p>2024- International trade Fair, Jan 05-07, 2024 at Tripuravasini Palace Grounds, Bengaluru.</p>
+                                            <!-- <a href="service-details.html" class="theme-btn">
+                                                View Details <i class="far fa-arrow-right"></i>
+                                            </a> -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="service-image">
+                                        <img src="../ITF/frontend/img/ITF-2024.jpg" alt="img">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<?php require_once 'inc/footer.php'; ?>
