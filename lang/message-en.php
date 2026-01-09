892b25d18c73533bee83adc4c83d2e1b8ed652ad
mr-perfect-white <dhilipkumarcit@gmail.com>
2026-01-09 14:30:04 +0530

    journey En & Kn Content updated

--- lang/message-en.php
+++ lang/message-en.php
@@ -2,7 +2,7 @@
    
 return [
     
-    // Header Menu
+    // <---------- Header Menu start----------->
     'home'=> 'Home',
     'about-itf'=> ' About ITF',
     'registrations'=> ' Registrations',
@@ -10,7 +10,35 @@ return [
     'gallery'=> 'Gallery',
     'contact'=> 'Contact',
     'department_login'=> 'Department Login',
-   
+
+     //<-------Header Menu End---------------->
+
+    //<----------Footer Menu start------------->
+
+    'footer_text'=> 'Lorem ipsum dolor sit amet, porro quisquam est, qui dolorem ipsum quia sed dolor sit amet, consectetur',
+    'footer_useful_link' => 'Useful Link',
+    'foot_email_us' => 'Email us',
+    'foot_location' => 'Location',
+    'foot_phone' => 'Phone',
+
+    //<----------Footer Menu End------------->
+
+    //<----------FAQ Section start------------->
+
+    'our_faq'=> 'Our FAQs',
+    'faq_title' => 'Frequently Asked Questions About Us',
+    'faq_ques_1' => '1.Event and venue Details',
+    'faq_ans_1' => 'MOITF will be from 23th-25th January 2026 .� Venue:Triupuravasini,Palace Grounds,Bengaluru',
+    'faq_ques_2' => '2.Is there any entry fee for visitors?',
+    'faq_ans_2' => 'MOITF is organised by Karnataka State Agriculture Department and entry for visitors is FREE.',
+    'faq_ques_3' => '3.How do Exhibitors/Conference Partipants register for the event?',
+    'faq_ans_3' => 'Exhibitors who wish to book a stall can visit the official website and navigate to Registrations Menu and seperate links are available for registrationa for various events of MOITF 2025.',
+    'faq_ques_4' => '4.Is there any offline mode of payment allowed for registrations?',
+    'faq_ans_4' => 'The registrations for exhibition stall booking, International Conference, Paid Food Stalls need to be completed by making ONLINE payment only . You will receive the confirmation email once you register and make the payment for an event whichever is applicable.',
+    'faq_ques_5' => '5.How do exhibitors contact fabricators to setup their stalls in the venue?',
+    'faq_ans_5' => 'Exhibitors can contact fabricators through the event organizers, who provide a list of approved vendors and setup guidelines.',
+
+    //<----------FAQ Section end------------->
 
 
     // Home page
@@ -31,6 +59,32 @@ return [
 
    // Journey page
   
+   'our_journey'=> 'Our Journey',
+   'itf_journey'=> 'ITF Journey',
+   'itf_journey_2017'=> '2017',
+   'itf_journey_2018'=> '2018',
+   'itf_journey_2019'=> '2019', 
+   'itf_journey_2023'=> '2023',
+   'itf_journey_2024'=> '2024',
+   'itf_journey_2025'=> '2025',
+   'organics_and_millets'=> 'Organics and Millets',
+   'millets_and_organics'=> 'Millets and Organics',
+   'agriculture_beyond_cultivation_and_empowering_farmers'=> 'Agriculture Beyond Cultivation and Empowering Farmers',
+   'itf_journey_para'=>'The Sixth edition of Agriculture beyond cultivation and empowering farmers , 
+                        2025 – International Trade Fair is organized by the Department of Agriculture,
+                        Government Of Karnataka. It will be a platform for farmers, farmer groups, domestic
+                        and international companies, central and state institutions in the organic and millet
+                        sector to connect and explore opportunities ain Agri-horticulture, processing, machinery a
+                        nd Agri-technology. The Government of Karnataka has been a leader in the promotion of
+                        organics and millets with the First Organics and Millets trade fair being held in 2017,
+                        and subsequently the second, third and fourth editions in 2018, 2019 ,2023 and 2024 respectively in Bengaluru.',
+   'itf_journey_2017_content'=> '2017-National Trade Fair during April 28-30, 2017 at Tripuravasini Palace Grounds, Bengaluru.',
+   'itf_journey_2018_content'=> '2018-International Trade Fair during Jan 19-21, 2018 at Tripuravasini Palace Grounds, Bengaluru.',
+   'itf_journey_2019_content'=> 'Wendell Berry said it best when he said that “eating is an agricultural act.” Our mission is to change the food system 
+                                 by providing a healthy, fresh alternative to industrial food.',
+   'itf_journey_2023_content'=> '2023- International trade Fair, Jan 18-20, 2023 at Tripuravasini Palace Grounds, Bengaluru.',
+   'itf_journey_2024_content'=> '2025- International trade Fair, Jan 05-07, 2024 at Tripuravasini Palace Grounds, Bengaluru.',
+   'itf_journey_2025_content'=> '2024- International trade Fair, Jan 05-07, 2024 at Tripuravasini Palace Grounds, Bengaluru.',
 
 
 ];
